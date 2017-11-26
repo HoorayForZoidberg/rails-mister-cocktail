4969fcdda1f8c3129d9578bad7b8e9c984a41710
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-26 20:38:57 +0100

    models pass all tests

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,16 @@
 class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :doses
+  before_destroy :check_for_cocktails
 
   validates :name, presence: true, uniqueness: true
+
+  private
+
+  def check_for_cocktails
+    unless self.cocktails.nil?
+      self.errors[:base] << "Cannot delete ingredient while it is used in cocktails."
+      return false
+    end
+  end
 end
