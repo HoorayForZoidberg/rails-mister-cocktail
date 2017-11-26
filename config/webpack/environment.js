e3d65aecba2b35e988b13c0461873497487fda0c
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-26 23:06:42 +0100

    fixed error in index.html re: cocktail_path param

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
 module.exports = environment
